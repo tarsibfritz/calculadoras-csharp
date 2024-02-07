007c04b0bed70975e9b0901799673e52db37f592
tarsibfritz <tarsilafritz0305@outlook.com>
2024-02-07 10:59:08 -0300

    update README.md

--- obj/Debug/net8.0/Trabalho.AssemblyInfo.cs
+++ obj/Debug/net8.0/Trabalho.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trabalho")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eac0d0a73ccef18398accc445b3dd2d9f72bae01")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trabalho")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trabalho")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
